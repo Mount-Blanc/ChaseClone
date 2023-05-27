480790e5035b3723ec7295161e7403efcbb3baa2
Mount-Blanc <mountblanc100@gmail.com>
2023-05-27 15:14:01 -0500

    added header navigation categories links

--- chaseclone/src/App.jsx
+++ chaseclone/src/App.jsx
@@ -6,7 +6,21 @@ function App() {
   return (
     <>
     <header>
-      <section>top</section>
+      <section>
+        <ul className='header-nav-categories-links'>
+          <li><a href=''>Personal</a></li>
+          <li><a href=''>Business</a></li>
+          <li><a href=''>Commercial</a></li>
+
+        </ul>
+      </section>
+      <section>
+        <ul  className='header-nav-categories-links'>
+          <li><a href=''>Schedule a meeting</a></li>
+          <li><a href=''>Customer service</a></li>
+          <li><a href=''>Espanol</a></li>
+        </ul>
+      </section>
       <secton>nav</secton>
     </header>
     </>
